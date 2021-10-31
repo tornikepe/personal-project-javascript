88cddded819a05881b57607c4a6640efd3618377
tornikepe <peitrishvilitornike@gmail.com>
2021-10-31 14:18:46 +0400

    Changes

--- 2-TypeScriptProject.ts
+++
@@ -1,9 +0,0 @@
-export interface dispatchMethod<T> {
-  dispatch(scenario: T): Promise<T>;
-}
-export interface errFuncInterface {
-  (storeBefore: any, step: any, result?: any): object;
-}
-export interface errFuncInterface2 {
-  (sb: any, step: any, res?: any): null;
-}
